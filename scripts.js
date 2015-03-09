ce1ab6cc944e8ff8d95e1cbf89e9850c84ceb8ef
Jennifer Wang <jlw2219@columbia.edu>
2015-03-08 21:31:36 -0400

    Created a basic map of New York, with no popups or markers.

---
+++ scripts.js
@@ -0,0 +1,5 @@
+var map = L.map('healthCentreMap').setView([40.714269, -74.005973], 13);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
