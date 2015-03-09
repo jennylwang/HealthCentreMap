8754c60df9e87a0cb7da1c2bced3f4f370e5c559
Jennifer Wang <jlw2219@columbia.edu>
2015-03-08 21:58:01 -0400

    Created a loop and mapped the location of every health centre with a marker using their longitude and latitude.

--- scripts.js
+++ scripts.js
@@ -3,3 +3,13 @@ var map = L.map('healthCentreMap').setView([40.714269, -74.005973], 13);
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+
+var mappingHealthCentres = healthCentreData.rows;
+
+for (var i=0; i<mappingHealthCentres.length; i++) {
+	var healthCentreLocation = mappingHealthCentres [i]; 
+	
+	var marker = L.marker([healthCentreLocation.latitude, healthCentreLocation.longitude]).addTo(map);
+	
+}
