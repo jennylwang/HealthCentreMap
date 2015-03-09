d61a3938bd140806a2dc7bf6278411836fff250a
Jennifer Wang <jlw2219@columbia.edu>
2015-03-08 21:32:22 -0400

    Added geocoded data for community health centres.

---
+++ data.js
@@ -0,0 +1,405 @@
+var healthCentreData= 
+{
+  "rows" : [
+    {
+      "NameOfBorough" : "Bronx",
+      "NameOfCenter" : "Bronx-Lebanon Hospital Center",
+      "CenterAddress" : "1276 Fulton Avenue, Suite 401-402",
+      "FullAddress" : "1276 Fulton Avenue, Suite 401-402, Bronx, NY 10456",
+      "longitude" : -73.90385,
+      "latitude" : 40.831747,
+      "Zip" : 10456,
+      "TelephoneNumber" : "(718) 466-6072"
+    },
+    {
+      "NameOfBorough" : "Bronx",
+      "NameOfCenter" : "Comprehensive Family Care Center",
+      "CenterAddress" : "1265 Franklin Avenue",
+      "FullAddress" : "1265 Franklin Avenue, Bronx, NY 10456",
+      "longitude" : -73.902303,
+      "latitude" : 40.831351,
+      "Zip" : 10456,
+      "TelephoneNumber" : "(718) 579-2500 "
+    },
+    {
+      "NameOfBorough" : "Bronx",
+      "NameOfCenter" : "Dr. Martin Luther King Jr. Health Center",
+      "CenterAddress" : "(Montefiore) 1621 Eastchester Road",
+      "FullAddress" : "(Montefiore) 1621 Eastchester Road, Bronx, NY 10461",
+      "longitude" : -73.846235,
+      "latitude" : 40.84449,
+      "Zip" : 10461,
+      "TelephoneNumber" : "(718) 503-7714 "
+    },
+    {
+      "NameOfBorough" : "Bronx",
+      "NameOfCenter" : "Montefiore Family Health Center",
+      "CenterAddress" : "360 East 193rd Street",
+      "FullAddress" : "360 East 193rd Street, Bronx, NY 10458",
+      "longitude" : -73.891386,
+      "latitude" : 40.863226,
+      "Zip" : 10458,
+      "TelephoneNumber" : " (718) 933-2400"
+    },
+    {
+      "NameOfBorough" : "Bronx",
+      "NameOfCenter" : "Montefiore Medical Group Comprehensive HCC",
+      "CenterAddress" : "305 East 161st Street",
+      "FullAddress" : "305 East 161st Street, Bronx, NY 10451",
+      "longitude" : -73.916583,
+      "latitude" : 40.824899,
+      "Zip" : 10451,
+      "TelephoneNumber" : "(718) 579-2597 "
+    },
+    {
+      "NameOfBorough" : "Bronx",
+      "NameOfCenter" : "Morris Heights Health Center",
+      "CenterAddress" : "Morris Heights Health Center",
+      "FullAddress" : "Morris Heights Health Center, Bronx, NY 10453",
+      "longitude" : -73.908755,
+      "latitude" : 40.846883,
+      "Zip" : 10453,
+      "TelephoneNumber" : "(718) 716-4400 "
+    },
+    {
+      "NameOfBorough" : "Bronx",
+      "NameOfCenter" : "Soundview Health Center",
+      "CenterAddress" : "731 White Plains Road",
+      "FullAddress" : "731 White Plains Road, Bronx, NY 10473",
+      "longitude" : -73.857957,
+      "latitude" : 40.820519,
+      "Zip" : 10473,
+      "TelephoneNumber" : "(718) 589-2232 "
+    },
+    {
+      "NameOfBorough" : "Bronx",
+      "NameOfCenter" : "Urban Health Plan",
+      "CenterAddress" : "1065 Southern Boulevard",
+      "FullAddress" : "1065 Southern Boulevard, Bronx, NY 10459",
+      "longitude" : -73.891854,
+      "latitude" : 40.82484,
+      "Zip" : 10459,
+      "TelephoneNumber" : " (718) 991-4833"
+    },
+    {
+      "NameOfBorough" : "Brooklyn",
+      "NameOfCenter" : "Bedford/Stuyvesant Family Health Center",
+      "CenterAddress" : "1413 Fulton Street",
+      "FullAddress" : "1413 Fulton Street, Brooklyn, NY 11216",
+      "longitude" : -73.944738,
+      "latitude" : 40.680028,
+      "Zip" : 11216,
+      "TelephoneNumber" : "(718) 636-4500"
+    },
+    {
+      "NameOfBorough" : "Brooklyn",
+      "NameOfCenter" : "Brooklyn Plaza Medical Center",
+      "CenterAddress" : "650 Fulton Street",
+      "FullAddress" : "650 Fulton Street, Brooklyn, NY 11217",
+      "longitude" : -73.979162,
+      "latitude" : 40.688272,
+      "Zip" : 11217,
+      "TelephoneNumber" : "(718) 596-9896"
+    },
+    {
+      "NameOfBorough" : "Brooklyn",
+      "NameOfCenter" : "Brownsville Multi-Service Family Health Center",
+      "CenterAddress" : "592 Rockaway Avenue",
+      "FullAddress" : "592 Rockaway Avenue, Brooklyn, NY 11212",
+      "longitude" : -73.909699,
+      "latitude" : 40.665628,
+      "Zip" : 11212,
+      "TelephoneNumber" : "(718) 345-5000 "
+    },
+    {
+      "NameOfBorough" : "Brooklyn",
+      "NameOfCenter" : "Community Health Care Network - CABS Health Center",
+      "CenterAddress" : "94-98 Manhattan Ave.",
+      "FullAddress" : "94-98 Manhattan Ave., Brooklyn, NY 11206",
+      "longitude" : -73.944687,
+      "latitude" : 40.705739,
+      "Zip" : 11206,
+      "TelephoneNumber" : "(718) 388-0390"
+    },
+    {
+      "NameOfBorough" : "Brooklyn",
+      "NameOfCenter" : "Community Health Care Network ",
+      "CenterAddress" : "Caribbean House Health Center",
+      "FullAddress" : "Caribbean House Health Center, Brooklyn, NY 11225",
+      "longitude" : -73.966265,
+      "latitude" : 40.632546,
+      "Zip" : 11225,
+      "TelephoneNumber" : "(718) 778-0198 "
+    },
+    {
+      "NameOfBorough" : "Brooklyn",
+      "NameOfCenter" : "Community Health Care Network ",
+      "CenterAddress" : "Dr. Betty Shabazz Health Center",
+      "FullAddress" : "Dr. Betty Shabazz Health Center, Brooklyn, NY 11208",
+      "longitude" : -74.007098,
+      "latitude" : 40.675903,
+      "Zip" : 11208,
+      "TelephoneNumber" : "(718) 277-8303 "
+    },
+    {
+      "NameOfBorough" : "Brooklyn",
+      "NameOfCenter" : "Ezra Medical Center",
+      "CenterAddress" : "571 McDonald Avenue",
+      "FullAddress" : "571 McDonald Avenue, Brooklyn, NY 11213",
+      "longitude" : -73.978844,
+      "latitude" : 40.640286,
+      "Zip" : 11213,
+      "TelephoneNumber" : "(718) 686-7600 "
+    },
+    {
+      "NameOfBorough" : "Brooklyn",
+      "NameOfCenter" : "ODA Primary Care Health Center",
+      "CenterAddress" : "14-16 Heyward Street",
+      "FullAddress" : "14-16 Heyward Street, Brooklyn, NY 11211",
+      "longitude" : -73.959456,
+      "latitude" : 40.6997,
+      "Zip" : 11211,
+      "TelephoneNumber" : "(718) 260-4600 "
+    },
+    {
+      "NameOfBorough" : "Brooklyn",
+      "NameOfCenter" : "Sunset Park Family Health Center",
+      "CenterAddress" : "150 55th Street",
+      "FullAddress" : "150 55th Street, Brooklyn, NY 11220",
+      "longitude" : -74.021233,
+      "latitude" : 40.646884,
+      "Zip" : 11220,
+      "TelephoneNumber" : "(718) 630-7095 "
+    },
+    {
+      "NameOfBorough" : "Brooklyn",
+      "NameOfCenter" : "Sunset Park Family Health Center",
+      "CenterAddress" : "9711 Third Avenue",
+      "FullAddress" : "9711 Third Avenue, Brooklyn, NY 11209",
+      "longitude" : -74.034145,
+      "latitude" : 40.615695,
+      "Zip" : 11209,
+      "TelephoneNumber" : "(718) 759-9126 "
+    },
+    {
+      "NameOfBorough" : "Brooklyn",
+      "NameOfCenter" : "Sunset Park Family Health Center Brooklyn-Chinese Family Health Center",
+      "CenterAddress" : "812 54th Street",
+      "FullAddress" : "812 54th Street, Brooklyn, NY 11220",
+      "longitude" : -74.005883,
+      "latitude" : 40.638655,
+      "Zip" : 11220,
+      "TelephoneNumber" : "(718) 686-2680 "
+    },
+    {
+      "NameOfBorough" : "Brooklyn",
+      "NameOfCenter" : "Sunset Park Family Health Center Family Physician Family Health Center",
+      "CenterAddress" : "5616 6th Avenue",
+      "FullAddress" : "5616 6th Avenue, Brooklyn, NY 11220",
+      "longitude" : -74.011742,
+      "latitude" : 40.64037,
+      "Zip" : 11220,
+      "TelephoneNumber" : "(718) 439-5440 "
+    },
+    {
+      "NameOfBorough" : "Brooklyn",
+      "NameOfCenter" : "Sunset Park Family Health Center Park Ridge Family Health Center",
+      "CenterAddress" : "6317 4th Avenue",
+      "FullAddress" : "6317 4th Avenue, Brooklyn, NY 11220",
+      "longitude" : -74.020203,
+      "latitude" : 40.639006,
+      "Zip" : 11220,
+      "TelephoneNumber" : "(718) 907-8100 "
+    },
+    {
+      "NameOfBorough" : "Brooklyn",
+      "NameOfCenter" : "Sunset Park Family Health Center",
+      "CenterAddress" : "220 13th Street",
+      "FullAddress" : "220 13th Street, Brooklyn, NY 11215",
+      "longitude" : -73.990442,
+      "latitude" : 40.667669,
+      "Zip" : 11215,
+      "TelephoneNumber" : "(718) 832-5980 "
+    },
+    {
+      "NameOfBorough" : "Brooklyn",
+      "NameOfCenter" : "Sunset Park Family Health Center",
+      "CenterAddress" : "9000 Shore Road",
+      "FullAddress" : "9000 Shore Road, Brooklyn, NY 11209",
+      "longitude" : -74.040335,
+      "latitude" : 40.62207,
+      "Zip" : 11209,
+      "TelephoneNumber" : "(718) 630-8878 "
+    },
+    {
+      "NameOfBorough" : "Brooklyn",
+      "NameOfCenter" : "Sunset Park Family Health Center",
+      "CenterAddress" : "514 49th Street",
+      "FullAddress" : "514 49th Street, Brooklyn, NY 11220",
+      "longitude" : -74.009544,
+      "latitude" : 40.645417,
+      "Zip" : 11220,
+      "TelephoneNumber" : "(718) 854-1851 "
+    },
+    {
+      "NameOfBorough" : "Manhattan",
+      "NameOfCenter" : "Betances Health Center",
+      "CenterAddress" : "280 Henry Street",
+      "FullAddress" : "280 Henry Street, Manhattan, NY 10002",
+      "longitude" : -73.983749,
+      "latitude" : 40.714017,
+      "Zip" : 10002,
+      "TelephoneNumber" : "(212) 227-8408 "
+    },
+    {
+      "NameOfBorough" : "Manhattan",
+      "NameOfCenter" : "Boriken Neighborhood Health Center",
+      "CenterAddress" : "2253 Third Avenue",
+      "FullAddress" : "2253 Third Avenue, Manhattan, NY 10035",
+      "longitude" : -73.937098,
+      "latitude" : 40.802158,
+      "Zip" : 10035,
+      "TelephoneNumber" : "(212) 289-6650 "
+    },
+    {
+      "NameOfBorough" : "Manhattan",
+      "NameOfCenter" : "Callen-Lorde Community Health Center",
+      "CenterAddress" : "356 West 18th Street",
+      "FullAddress" : "356 West 18th Street, Manhattan, NY 10011",
+      "longitude" : -74.001969,
+      "latitude" : 40.74287,
+      "Zip" : 10011,
+      "TelephoneNumber" : "(212) 271-7200 "
+    },
+    {
+      "NameOfBorough" : "Manhattan",
+      "NameOfCenter" : "Charles B. Wang Community Health Center",
+      "CenterAddress" : "268 Canal Street",
+      "FullAddress" : "268 Canal Street, Manhattan, NY 10013",
+      "longitude" : -74.001083,
+      "latitude" : 40.718848,
+      "Zip" : 10013,
+      "TelephoneNumber" : "(212) 379-6988 "
+    },
+    {
+      "NameOfBorough" : "Manhattan",
+      "NameOfCenter" : "Community Health Care Network",
+      "CenterAddress" : "79 Madison Avenue",
+      "FullAddress" : "79 Madison Avenue, Manhattan, NY 10016",
+      "longitude" : -73.985695,
+      "latitude" : 40.744008,
+      "Zip" : 10016,
+      "TelephoneNumber" : "(212) 366-4500 "
+    },
+    {
+      "NameOfBorough" : "Manhattan",
+      "NameOfCenter" : "Heritage Health Care",
+      "CenterAddress" : "1727 Amsterdam Avenue",
+      "FullAddress" : "1727 Amsterdam Avenue, Manhattan, NY 10031",
+      "longitude" : -73.9474,
+      "latitude" : 40.825448,
+      "Zip" : 10031,
+      "TelephoneNumber" : "(212) 862-0054 "
+    },
+    {
+      "NameOfBorough" : "Manhattan",
+      "NameOfCenter" : "Institute for Urban Family Health",
+      "CenterAddress" : "16 East 16th Street",
+      "FullAddress" : "16 East 16th Street, Manhattan, NY 10003",
+      "longitude" : -73.992477,
+      "latitude" : 40.737146,
+      "Zip" : 10003,
+      "TelephoneNumber" : "(212) 633-0800 "
+    },
+    {
+      "NameOfBorough" : "Manhattan",
+      "NameOfCenter" : "Ryan/Chelsea-Clinton Community Health Center",
+      "CenterAddress" : "645 Tenth Avenue",
+      "FullAddress" : "645 Tenth Avenue, Manhattan, NY 10036",
+      "longitude" : -73.993916,
+      "latitude" : 40.761982,
+      "Zip" : 10036,
+      "TelephoneNumber" : "(212) 265-4500 "
+    },
+    {
+      "NameOfBorough" : "Manhattan",
+      "NameOfCenter" : "Settlement Health & Medical Services",
+      "CenterAddress" : "212 East 106th Street",
+      "FullAddress" : "212 East 106th Street, Manhattan, NY 10029",
+      "longitude" : -73.944499,
+      "latitude" : 40.791383,
+      "Zip" : 10029,
+      "TelephoneNumber" : "(212) 360-2661"
+    },
+    {
+      "NameOfBorough" : "Manhattan",
+      "NameOfCenter" : "William F. Ryan Community Health Center",
+      "CenterAddress" : "110 West 97th Street",
+      "FullAddress" : "110 West 97th Street, Manhattan, NY 10025",
+      "longitude" : -73.967269,
+      "latitude" : 40.793614,
+      "Zip" : 10025,
+      "TelephoneNumber" : "(212) 316-7906 "
+    },
+    {
+      "NameOfBorough" : "Queens",
+      "NameOfCenter" : "Charles B. Wang Community Health Center",
+      "CenterAddress" : "136-26 37th Ave - 2nd Fl",
+      "FullAddress" : "136-26 37th Ave - 2nd Fl, Queens, NY 11345",
+      "longitude" : -73.82348,
+      "latitude" : 40.797332,
+      "Zip" : 11345,
+      "TelephoneNumber" : "(718) 886-1200 "
+    },
+    {
+      "NameOfBorough" : "Queens",
+      "NameOfCenter" : "Community Health Care Network",
+      "CenterAddress" : "97-04 Sutphin Blvd",
+      "FullAddress" : "97-04 Sutphin Blvd, Queens, NY 11435",
+      "longitude" : -73.825305,
+      "latitude" : 40.712075,
+      "Zip" : 11435,
+      "TelephoneNumber" : "(718) 657-7088"
+    },
+    {
+      "NameOfBorough" : "Queens",
+      "NameOfCenter" : "The Floating Hospital",
+      "CenterAddress" : "41-40 27th Street",
+      "FullAddress" : "41-40 27th Street, Queens, NY 11101",
+      "longitude" : -73.938738,
+      "latitude" : 40.751367,
+      "Zip" : 11101,
+      "TelephoneNumber" : "(718) 784-2240"
+    },
+    {
+      "NameOfBorough" : "Queens",
+      "NameOfCenter" : "Joseph P. Addabbo Family Health Center",
+      "CenterAddress" : "6200 Beach Channel Drive",
+      "FullAddress" : "6200 Beach Channel Drive, Queens, NY 11692",
+      "longitude" : -73.793273,
+      "latitude" : 40.5923,
+      "Zip" : 11692,
+      "TelephoneNumber" : "(718) 634-2155 "
+    },
+    {
+      "NameOfBorough" : "Queens",
+      "NameOfCenter" : "Project Samaritan Health Services /",
+      "CenterAddress" : "137-50 Jamaica Avenue",
+      "FullAddress" : "137-50 Jamaica Avenue, Queens, NY 11435",
+      "longitude" : -73.818933,
+      "latitude" : 40.709082,
+      "Zip" : 11435,
+      "TelephoneNumber" : "(718) 298-5100 "
+    },
+    {
+      "NameOfBorough" : "Staten Island",
+      "NameOfCenter" : "Staten Island ",
+      "CenterAddress" : "235 Port Richmond Avenue",
+      "FullAddress" : "235 Port Richmond Avenue, Staten Island, NY 10302",
+      "longitude" : -74.13517,
+      "latitude" : 40.635261,
+      "Zip" : 10302,
+      "TelephoneNumber" : "(718) 876-1732"
+    }
+  ]
+}
